b005abf5bec02a9f2030662c529e39c888fbd414
HASSAN <hassan.aburesha@gmail.com>
2022-12-15 19:22:45 +0100

    part4/5(WIP)

--- db.js
+++ db.js
@@ -5,11 +5,39 @@ const db = spicedPg(
     `postgres:${SQL_USER}:${SQL_PASSWORD}@localhost:5432/petition`
 );
 
-module.exports.getSigImg = (dbSigID) => {
+module.exports.getSigImg = (user_id) => {
     return db.query(`SELECT signature FROM signatures WHERE id = $1;`, [
-        dbSigID,
+        user_id,
     ]);
 };
+
+// module.exports.getEmailandPassword = (loginEmail) => {
+//     return db.query(`SELECT email, password, id FROM users WHERE email = $1 `, [
+//         loginEmail,
+//     ]);
+// };
+
+module.exports.getEmailandPassword = (loginEmail) => {
+    return db.query(
+        `SELECT email, password, signature FROM users FULL OUTER JOIN signatures ON users.id = signatures.user_id WHERE email = $1 `,
+        [loginEmail]
+    );
+};
+
+module.exports.getUserDataAll = () => {
+    return db.query(
+        `SELECT users.firstname, users.lastname, user_profiles.city, user_profiles.age, user_profiles.homepage, 
+        * FROM users
+        FULL OUTER JOIN user_profiles ON users.id = user_profiles.user_id;`
+    );
+};
+
+/* ---------------------------------
+INSERT INTO users (name, age, oscars)
+VALUES
+ON CONFLICT (name or user_id)
+DO UPDATE bla bla
+---------------------------------- */
 
 module.exports.addUserData = (firstName, lastName, email, password) => {
     return db.query(
@@ -19,11 +47,11 @@ module.exports.addUserData = (firstName, lastName, email, password) => {
     );
 };
 
-module.exports.addProfiles = (city, age, homepage) => {
+module.exports.addProfiles = (city, age, homepage, user_id) => {
     return db.query(
-        `INSERT INTO users (city, age, homepage) 
+        `INSERT INTO user_profiles (city, age, homepage, user_id) 
       VALUES ($1, $2, $3, $4)`,
-        [city, age, homepage]
+        [city, age, homepage, user_id]
     );
 };
 
--- server.js
+++ server.js
@@ -1,25 +1,16 @@
 const express = require("express");
 const app = express();
-
 const path = require("path");
-
 const db = require("./db");
 const cookieSession = require("cookie-session");
-
-// Handlebars Setup
 const { engine } = require("express-handlebars");
 const encrypt = require("./encrypt");
-
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
-// End of setup
-
 const urlEncodedMiddleware = express.urlencoded({ extended: false });
 app.use(urlEncodedMiddleware);
-
 app.use(express.static("./views"));
 app.use(express.static("./public"));
-
 app.use(
     cookieSession({
         secret: `I'm always angry.`,
@@ -28,11 +19,13 @@ app.use(
 );
 
 app.get("/register", (req, res) => {
+    // SET FORM  USER: {} & IN THE HTML NAME IS FIRSTNAME & SET {{VALUE="PREDEFINED"}}
     res.render("register", {
         layout: "main",
         helpers: {
             formStyles: "registerStyles.css",
             favicon: "favicon.ico",
+            //User: {firstname: "", lastname:""} then in the hbfile {{#users & then addess its variables inside the loo}}
         },
     });
 });
@@ -46,9 +39,7 @@ app.post("/register", (req, res) => {
             return db.addUserData(firstName, lastName, email, hashedPWD);
         })
         .then(({ rows }) => {
-            const signatureRowId = rows[0].id;
-            req.session.signerID = signatureRowId;
-            console.log("req.session.signerID", req.session.signerID);
+            req.session.user_id = rows[0].id;
             res.redirect("/profile");
         })
         .catch((err) => console.log("error in addUserData: ", err));
@@ -56,33 +47,26 @@ app.post("/register", (req, res) => {
 
 app.get("/profile", (req, res) => {
     res.render("profile", {
-        //whatever you specify here,
-        // it will be used as a body in the main.handlebars!
-        // (i.e. home.handlebars)
         layout: "main",
-        // projects: projectsList,
-        // showImage: true,
         helpers: {
             formStyles: "profileStyles.css",
             favicon: "favicon.ico",
         },
     });
-    //
 });
 
 app.post("/profile", (req, res) => {
+    const user_id = req.session.user_id;
     const { age, city, homepage } = req.body;
-    db.addProfiles(age, city, homepage);
+
+    db.addProfiles(age, city, homepage, user_id).then(() =>
+        res.redirect("/signers")
+    );
 });
 
 app.get("/login", (req, res) => {
     res.render("login", {
-        //whatever you specify here,
-        // it will be used as a body in the main.handlebars!
-        // (i.e. home.handlebars)
         layout: "main",
-        // projects: projectsList,
-        // showImage: true,
         helpers: {
             formStyles: "loginStyles.css",
             favicon: "favicon.ico",
@@ -92,6 +76,32 @@ app.get("/login", (req, res) => {
 });
 
 app.post("/login", (req, res) => {
+    const { loginEmail, loginPassword } = req.body;
+
+    db.getEmailandPassword(loginEmail).then(({ rows }) => {
+        if (rows[0]) {
+            console.log("getbyEmail:", rows[0]);
+            encrypt
+                .compare(loginPassword, rows[0].password)
+                .then((passedTest) => {
+                    if (passedTest) {
+                        res.redirect("/thanks");
+                        // console.log(
+                        //     "passTest & Signed?",
+                        //     passedTest,
+                        //     req.session.signed
+                        // );
+                        // if(signed)
+                        // res.redirect("/thanks");
+                    } else {
+                        res.redirect("/login");
+                    }
+                });
+        } else {
+            res.redirect("/register");
+        }
+    });
+
     // First check by the email if the user exists in your Database
     // If he/she exists then compare if the password matches
     // Go to the Signatures Table to see if this user already signed
@@ -117,57 +127,41 @@ app.get("/petition", (req, res) => {
 
 app.post("/petition", (req, res) => {
     // console.log(req.body);
-
-    const { signature, user_id } = req.body;
+    //user_id & signed true or false is accessed globally from the set cookies
+    const user_id = req.session.user_id;
+    const { signature } = req.body;
 
     db.addSignature(signature, user_id)
-        .then(({ rows }) => {
-            const signatureRowId = rows[0].id;
-            req.session.signerID = signatureRowId;
+        .then(() => {
+            req.session.signed = true;
+            console.log("Booleab Signed?: ", req.session.signed);
             res.redirect("/thanks");
         })
         .catch((err) => console.log("err in addSignature: ", err));
 });
 
-// })
-
-// Save first name, last name, and signature to Postgres SQL table
-
 app.get("/signers", (req, res) => {
-    res.render("signers", {
-        //whatever you specify here,
-        // it will be used as a body in the main.handlebars!
-        // (i.e. home.handlebars)
-        layout: "main",
-        // projects: projectsList,
-        // showImage: true,
-        // helpers: {
-        //     getnewstyle: "/style.css",
-        // },
+    // should also render the user with his signature (maybe also print certificate?)
+    db.getUserDataAll().then(({ rows }) => {
+        res.render("signers", {
+            layout: "main",
+            signers: rows,
+            helpers: {
+                formStyles: "signersStyles.css",
+                favicon: "favicon.ico",
+            },
+        });
     });
 });
 
 app.get("/thanks", (req, res) => {
-    if (req.session.signerID) {
-        const dbSigID = req.session.signerID;
-        db.getSigImg(dbSigID).then(({ rows }) => {
-            // console.log("rows:", rows);
-            const sigDataURL = rows[0].signature;
-            // console.log("sigDataURL:", sigDataURL);
-
-            res.render("thanks", {
-                //whatever you specify here,
-                // it will be used as a body in the main.handlebars!
-                // (i.e. home.handlebars)
-                layout: "main",
-                // projects: projectsList,
-                // showImage: true,
-                sigSrcURL: sigDataURL,
-            });
+    if ((req.session.signed = true)) {
+        res.render("thanks", {
+            layout: "main",
         });
     } else {
         res.redirect("/petition");
     }
 });
 
-app.listen(process.env.PORT || 3000, console.log("running at 3000"));
+app.listen(process.env.PORT || 3000, console.log(".🧨.🧨.🧨.🧨"));
