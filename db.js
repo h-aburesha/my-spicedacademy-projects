9aa75c72ffc52fb9efc63d12794ba7673f447d7a
HASSAN <hassan.aburesha@gmail.com>
2022-12-20 15:51:22 +0100

    Env variables

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const { SQL_USER, SQL_PASSWORD } = process.env; // add a .env file next to the db.js file with your PostgreSQL credentials
 const spicedPg = require("spiced-pg");
 const db = spicedPg(
-    `postgres:${SQL_USER}:${SQL_PASSWORD}@localhost:5432/petition`
+    process.env.DATABASE_URL ||
+        `postgres:${SQL_USER}:${SQL_PASSWORD}@localhost:5432/petition`
 );
 
 module.exports.getSigImg = (user_id) => {
